04967d7bf6b4a944ae46d73d3bd67c20f3216101
Wallyson Galvão <wallyson.galvao@gmail.com>
2018-04-13 16:47:19 -0300

    Basic Template

---
+++ Basic Template/gulpfile.js
@@ -0,0 +1,24 @@
+// Carregando o gulp e os plugins através da função `require` do nodejs
+// const gulp = require('gulp');
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('webserver', function () {
+  browserSync.init({
+    cors: true,
+    server: {
+      baseDir: "./",
+      middleware: function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Headers', '*');
+        next();
+      }
+    }
+  });
+});
+
+gulp.task('default', function () {
+  gulp.start('webserver');
+});
